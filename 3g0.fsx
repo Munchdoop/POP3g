f73bf5a6e3c2cdf95ad6be7dec57c58f2e0727ae
Munchdoop <44480498+Munchdoop@users.noreply.github.com>
2020-09-17 15:30:26 +0200

    e)

--- 3g0.fsx
+++ 3g0.fsx
@@ -2,7 +2,6 @@
 //opgave 3g0 a)
 /// <summary> A function has been made that sums all the numbers from i=1 to n<\summary>
 /// <param>
-/// <remarks> 
 /// <returns> The function return the sum of all the numbers </returns> 
 let sum n:int =
     if n < 1 then 
@@ -18,6 +17,9 @@ let sum n:int =
 //printfn "%i" (sum(8))
 
 //3g0 b)
+/// <summary> 
+/// <param>
+/// <returns>
 //The function returns the same result as the summation of all the numbers (1 + 2 + 3..+ n)
 let simpleSum n:int =
     if n < 0 then 
@@ -30,14 +32,28 @@ let simpleSum n:int =
 //printfn "%i" (simpleSum(8))
 
 //3g0 c)
+/// <summary> 
+/// <param>
+/// <returns>
 printf "Enter a Number:"
 let a = int (System.Console.ReadLine())
 printfn "sum = %i, simpleSum = %i" (sum(a)) (simpleSum(a))
 
 //3g0 d) 
+/// <summary> 
+/// <param>
+/// <returns>
 printfn "n sum simplesum"
 for i = 1 to 10 do 
     printfn "%d | %d | %d" i (sum(i)) (simpleSum(i)) 
 
 //3g0 e)
-(**)
+(* 
+The largest n both functions can calculate is 46340. because simplesum multiplies input.
+therefore the largest value must be lower than the square value of int32 max value. 
+However the sum function can calculate up to 65535 because it uses addition instead of multiplication
+
+Yes the functions can be modified by using uint64 instead of int32 which is the default in F#. We can use
+uint64 because we do not have any negative values
+
+*)
