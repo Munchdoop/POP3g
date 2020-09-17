d5c07fefbc043f988ddde7136d62f0c196e85521
Munchdoop <44480498+Munchdoop@users.noreply.github.com>
2020-09-17 13:27:03 +0200

    created a mock-up of 3g0

---
+++ 3g0.fsx
@@ -0,0 +1,10 @@
+//HUSK DOCUMENTATION (summary, param, result)
+
+let sum (n:int) =
+    if n < 1 then 0
+    else
+        let mutable i = 1
+        while i < n do
+            let mutable s = i + n
+            i <- i+1
+    s 
