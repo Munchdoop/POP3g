46b964d68eaa93b4f0d07d16d3d7264f76064010
Munchdoop <44480498+Munchdoop@users.noreply.github.com>
2020-09-17 14:05:02 +0200

    summary

--- 3g0.fsx
+++ 3g0.fsx
@@ -1,14 +1,19 @@
 //HUSK DOCUMENTATION (summary, param, result)
-//a
+//opgave 3g0 a)
+/// <summary> Vi har lavet en funktion der lægger alle tal i talrækken sammen til og med n<\summary>
+/// param
+/// <result> 
 let sum n:int =
     if n < 1 then 
         0
     else
         let mutable s = 0
         let mutable i = 1 //counter 
-        while i < n do
+        while i <= n do
             s <- s + i 
             i <- i+1
         s 
 
-printfn "%i" (sum(8))
+printfn "%i" (sum(-25))
+
+//3g0 b)
