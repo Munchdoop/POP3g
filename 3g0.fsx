cecc0d6159b6697c03dc47fca6483b87c2f89ecf
Munchdoop <44480498+Munchdoop@users.noreply.github.com>
2020-09-19 13:58:35 +0200

    Update 3g0.fsx
    param changed in 3g0.fsx

--- 3g0.fsx
+++ 3g0.fsx
@@ -1,9 +1,9 @@
 //HUSK DOCUMENTATION (summary, param, result)
 //opgave 3g0 a)
 /// <summary> A function has been made that sums all the numbers from i=1 to n</summary>
-/// <param> name="n"> input size. </param>
-/// <param> name="s"> variable to store summation. </param>
-/// <param> name="i"> counter in while-loop. </param>
+/// <param name="n"> input size. </param>
+/// <param name="s"> variable to store summation. </param>
+/// <param name="i"> counter in while-loop. </param>
 /// <returns> The function return the sum of all the numbers </returns> 
 let sum n:int =
     if n < 1 then 
@@ -18,8 +18,8 @@ let sum n:int =
 
 //3g0 b)
 /// <summary> We have made a function which uses the formula in figure 3 to find to summation of n </summary>
-/// <param> name="n"> input size. </param>
-/// <param> name="s"> variable to store result of formula in. </param>
+/// <param name="n"> input size. </param>
+/// <param name="s"> variable to store result of formula in. </param>
 /// <returns> The function returns the summation of n, the same result as in the sum function </returns>
 //The function returns the same result as the summation of all the numbers (1 + 2 + 3..+ n)
 let simpleSum n:int =
@@ -32,7 +32,7 @@ let simpleSum n:int =
 
 //3g0 c)
 /// <summary> This program asks the user for af Number and returns the summation calculated in sum og simpleSum </summary>
-/// <param> name="a"> This takes the input the user writes in console. </param>
+/// <param name="a"> This takes the input the user writes in console. </param>
 /// <returns> The program returns the values of the summation of n calculated in the sum og simpleSum function </returns>
 printf "Enter a Number:"
 let a = int (System.Console.ReadLine())
@@ -40,7 +40,7 @@ printfn "sum = %i, simpleSum = %i" (sum(a)) (simpleSum(a))
 
 //3g0 d) 
 /// <summary> This program prints a table that shows n from 1 to 10 and its sum with both functions </summary>
-/// <param> name="1"> this is an index in for-loop </param>
+/// <param name="1"> this is an index in for-loop </param>
 /// <returns> This program returns det sum of n in a table using both functions made above </returns>
 printfn " n |sum |simplesum"
 for i = 1 to 10 do 
